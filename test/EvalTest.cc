556249c19d2290ecbf4144c8918f7ede33487e8b
Jason Mittertreiner <jmittert@uwaterloo.ca>
2017-02-10 13:25:39 -0500

    Added some tests

--- test/EvalTest.cc
+++ test/EvalTest.cc
@@ -1,5 +1,36 @@
 #include "UnitTest++/UnitTest++.h"
 #include "eval.hh"
+#include "parser_driver.hh"
 
 SUITE(Eval) {
+  TEST(simple) {
+    parser_driver driver;
+    driver.parse("../test/simple.stg");
+    Prog* p = driver.p;
+    int res = eval(*p);
+    CHECK_EQUAL(0, res);
+  }
+
+  TEST(simple2) {
+    parser_driver driver;
+    driver.parse("../test/simple2.stg");
+    Prog* p = driver.p;
+    int res = eval(*p);
+    CHECK_EQUAL(1, res);
+  }
+  TEST(op) {
+    parser_driver driver;
+    driver.parse("../test/op.stg");
+    Prog* p = driver.p;
+    int res = eval(*p);
+    CHECK_EQUAL(3, res);
+  }
+
+  TEST(func) {
+    parser_driver driver;
+    driver.parse("../test/func.stg");
+    Prog* p = driver.p;
+    int res = eval(*p);
+    CHECK_EQUAL(6, res);
+  }
 }
